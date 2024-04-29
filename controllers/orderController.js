28d424b9999ebf435742f3bd25cedf519f5d5c6a
shadoeee <balaganesh541@gmail.com>
2024-04-30 02:45:58 +0530

    check

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -7,8 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order for frontend
 const placeOrder = async (req, res) => {
 
-    // const frontend_url = "http://localhost:5174"
-    const frontend_url = "https://euphonious-bombolone-7b0d91.netlify.app/"
+    const frontend_url = "http://localhost:5173"
 
     try {
         const newOrder = new orderModel({
